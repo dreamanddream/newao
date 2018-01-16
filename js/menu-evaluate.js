fb95e0386b6f9b45e10f743f78c9fcefb448bcdc
dreamanddream <1310335280@qq.com>
2018-01-16 15:09:01 +0800

    lastversion

--- js/menu-evaluate.js
+++ js/menu-evaluate.js
@@ -1,43 +1,56 @@
 $(function () {
-   goodBad();
-    backTop();
+    evaluate();
 })
+function evaluate() {
+    var menu_content=$("#menu-content");
+    var good=menu_content.find(".good");
+    var bad=menu_content.find(".bad");
+    var tan=$("#tan");
+    var mask=$(".mask");
+    var quxiao=tan.find(".quxiao");
+    var queding=tan.find(".queding");
+    var ul =$("#menu-content").find("ul");
 
-/*好评和差评*/
-    function goodBad() {
-        var menu_content=$("#menu-content");
-        var good=menu_content.find(".good");
-        var bad=menu_content.find(".bad");
-        var tan=$("#tan");
-        var mask=$(".mask");
-        var quxiao=tan.find(".quxiao");
-        var flag=1;
-        /*好评*/
-        if(flag===1){
-            good.on("click",function () {
-                $(this).attr("src","images/good.png");
-                flag=0;
-            })
+    /*点击取消*/
+    quxiao.on("click",function () {
+        mask.hide();
+        tan.hide();
+    })
+    good.on("click",function () {
+        if($(this).children().attr("src")=="images/good-grey.png"){
+            $(this).children().attr("src","images/good.png");
+            $(this).next().children().attr("src","images/bad.png")
         }
-        /*差评*/
-        bad.on("click",function () {
-            mask.show();
-            tan.show();
-            if(flag==0){
-                $(this).parent().prev().children().attr("src","images/bad.png");
-                $(this).attr("src","images/good-bad.png");
-                flag=1;
+    });
+    var bad_id;
+    bad.on("click",function () {
+        bad_id=$(this).attr("data-id");
+        /*$("#badID").val(bad_id);*/
+        //alert(id)
+        if( $('#ul-'+bad_id).children().children(".bad-img").attr("src")!="images/bad.png"){
+            return false;
+        }
+        mask.show();
+        tan.show();
+
+    });
+    queding.on("click",function () {
+        /*var id=$("#badID").val();*/
+        /*console.log(id);*/
+        if( $('#ul-'+bad_id).children().children(".bad-img").attr("src")=="images/bad.png"){
+            $('#ul-'+bad_id).children().children(".bad-img").attr("src","images/good-bad.png");
+            if($('#ul-'+bad_id).children(".good").children().attr("src")=="images/good.png"){
+                $('#ul-'+bad_id).children(".good").children().attr("src","images/good-grey.png");
             }
 
-        })
-        quxiao.on("click",function () {
-            mask.hide();
-            tan.hide();
-        })
-        mask.on("click",function () {
-            mask.hide();
-            tan.hide();
-        })
-    }
+        }else{
+            $('#ul-'+bad_id).children().children(".bad-img").attr("src","images/bad.png");
+        }
+        // bad_id=null;
+        // $('#ul-'+id).children(".good").children().attr("src","images/bad.png");
+        mask.hide();
+        tan.hide();
+    });
+}
 
 
--- js/question.js
+++ js/question.js
@@ -10,12 +10,16 @@ $(function () {
 //单选公共点击
 function click(id,children) {
     var images = $(id).find(children);
+    //localStorage.radio1="./images/check.png";
+    // localStorage.radio2="./images/check.png";
+    // localStorage.radio3="./images/check.png"x ;
     images.on("click",function () {
             if($(this).find("img").attr("src")=="./images/check.png"){
                 /*  $(this).find("img").attr("src","./images/no-check.png");
                 $(this).siblings().find("img").attr("src","./images/check.png");*/
             }else {
                 $(this).find("img").attr("src","./images/check.png");
+                //localStorage.radio1
                 $(this).siblings().find("img").attr("src","./images/no-check.png");
                 if($(this).attr("data-result")){
                     if($(this).attr("data-result")=="check"){
@@ -56,3 +60,73 @@ function clicks(id) {
     })
 }
 //显示多行文本框
+
+
+
+
+$.ajax({
+    type:"get",  //post
+    url:"接口地址",
+    data:{
+        username:"zhabg",
+        password:"123"
+    },
+    dataType:"json",
+    success:function(ret){
+        if(ret && ret.status =="000000"){
+            //DOM炒作
+            var html="";
+            for(var i=0;i<ret.list.length;i++){
+                html+='<img src="'+ret.list[i].url+'"/>'
+            }
+            $("#listImg").append(html)
+        }else {
+            alert(ret.message)
+        }
+    },
+    error:function(ret){
+
+    }
+})
+function clickEvent() {
+    localStorage.user1="不限";
+    localStorage.user2="不限";
+    localStorage.user3="不限";
+    $("#end span").on("click",function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        localStorage.user1=$(this).html();
+    })
+    $("#date span").on("click",function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        localStorage.user2=$(this).html();
+    })
+    $("#price span").on("click",function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        localStorage.user3=$(this).html();
+    })
+
+    $("#btn").on("click",function () {
+        skip("./自助游搜索页列表.html");
+
+        $.ajax({
+            url:"",
+            type:'POST',
+            dataType:'json',
+            data:{
+                user1:localStorage.user1,
+                user2:localStorage.user2,
+                user3:localStorage.user3,
+                user4:$("#insert").val()
+            }
+        }).done( function (data ) {
+            if ( data.success ) {
+                skip("./自助游搜索页.html");
+
+            }else {
+
+
+            }
+
+        } )
+    })
+}
